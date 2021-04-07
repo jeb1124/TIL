4e69a80e3b90cbdf8700f08eba47034e8916d899
jeb1124 <81003491+jeb1124@users.noreply.github.com>
2021-04-07 16:30:44 +0900

    Add files via upload

---
+++ web20_FactoryPattern/src/pattern/action/Controller.java
@@ -0,0 +1,6 @@
+package pattern.action;
+
+public interface Controller {
+	String requestHandle();//public abstract가 앞에 생략되어 있다.
+	
+}
---
+++ web20_FactoryPattern/src/pattern/action/UpdateController.java
@@ -0,0 +1,19 @@
+package pattern.action;
+
+public class UpdateController implements Controller{
+
+	@Override
+	public String requestHandle() {
+		System.out.println("UpdateController..update logic....");
+		/*
+		 * 1. 폼값 받아서
+		 * 2. VO 객체 생성
+		 * 3. DAO리턴 받아서
+		 * 4. 비지니스 로직 호출
+		 * 5. 바인딩
+		 * 6. PATH를 리턴
+		 */
+		return "update_result.jsp";
+	}
+
+}

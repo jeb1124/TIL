871c30f3cde6205fe0e31af1333c03d2f160957e
jeb1124 <81003491+jeb1124@users.noreply.github.com>
2021-04-13 16:43:12 +0900

    Add files via upload

---
+++ web27_FactoryPattern_Ajax/src/servlet/controller/IdListController.java
@@ -0,0 +1,29 @@
+package servlet.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import servlet.model.MemberDAOImpl;
+import servlet.model.MemberVO;
+
+public class IdListController implements Controller{
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			request.setAttribute("list", MemberDAOImpl.getInstance().getIdList());
+		} catch (SQLException e) {
+			
+		}
+		return new ModelAndView("show_idList.jsp");
+	}
+}
+/*
+ * Arraylist list=DAO의 모든 ID값의 리스트를 받아온다...getIdList();
+ * Attribute에 list라는 이름으로 바인딩
+ * path = show_idList.jsp
+ */

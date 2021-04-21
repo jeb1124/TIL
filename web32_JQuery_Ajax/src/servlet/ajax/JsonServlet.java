b74d00e2e4347321c7d9fc54b7591e4f79134d6e
jeb1124 <81003491+jeb1124@users.noreply.github.com>
2021-04-21 11:19:56 +0900

    Add files via upload

---
+++ web32_JQuery_Ajax/src/servlet/ajax/JsonServlet.java
@@ -0,0 +1,50 @@
+package servlet.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import domain.Weather;
+
+
+@WebServlet("/JsonServlet")
+public class JsonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		//1. JSON객체를 하나 생성... json.jar 가 있어야 한다.
+		JSONObject json = new JSONObject();
+		
+		json.put("region", "Busan");
+		json.put("wind", 34);
+		json.put("temp", 27);
+		json.put("rain", 10);
+		
+		//2. Weather 객체를 생성
+		Weather weather = new Weather();
+		weather.setRegion("seoul");
+		weather.setWind(44);
+		weather.setTemp(23);
+		weather.setRain(55);
+		
+		//3. 객체를 json에 put
+		json.put("weather", weather);
+		
+		//4. 객체를 전송
+		out.print(json);
+		
+		
+		
+	}
+
+}
